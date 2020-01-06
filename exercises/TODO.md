f6916deb35519b34bb9455ab20c29d34ba1fe270
David Schneider <david.jay.schneider@gmail.com>
2020-01-06 09:41:08 -0500

    Updates folder organization

---
+++ exercises/TODO.md
@@ -0,0 +1,15 @@
+
+- Be able to read stuff from the address (the URL) 
+    - Test with console.log
+- Get joke from the Chuck Norris API
+    - Pull from the API address
+    - Use a third-party library to make Request
+
+1. Browser makes request
+2. Node receives request
+3. Node makes request
+4. Node sends response
+5. Node receives response
+6. Node mashes up text with the joke from Churck API
+7. Node sends response
+8. Browser receives response
---
+++ exercises/index.js
@@ -0,0 +1,64 @@
+const http = require("http");
+const fs = require("fs");
+const faker = require("faker");
+
+const madlib = fs.readFileSync("templates/madlib.html").toString();
+
+let counter = 0;
+
+const server = http.createServer((req, res) => {
+    counter += 1;
+    const url = req.url.charAt(1).toUpperCase() + req.url.slice(2);
+    const splitUrl = url.split("/");
+    const specialGreetings = {
+        Oakley: "How wonderfully splendid it is to be in your presence again!",
+        Chris: "Oh hey...",
+        Milla: "Hello, Milla."
+    };
+    
+    const hackerKeywords = ["Adjective", "Noun", "Verb"];
+
+    let newTemplate = madlib
+        .replace("***NAME***", splitUrl[0])
+        .replace("***PLACE***", splitUrl[1]);
+    let returnString = "";
+
+    if (Object.keys(specialGreetings).includes(url)) {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        returnString = `<h1>${specialGreetings[url]}</h1>`;
+    } else if (splitUrl.length == 1) {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        returnString = `<h1>Hello, ${url}!</h1>`;
+    } else if (hackerKeywords.includes(splitUrl[0])) {
+        let result = "";
+        for (item of splitUrl) {
+            const word = item.toLowerCase();
+            if (word === "adjective") {
+                result += `${faker.hacker.adjective()} `;
+            } else if (word === "noun") {
+                result += `${faker.hacker.noun()} `;
+            } else if (word === "verb") {
+                result += `${faker.hacker.verb()} `;
+            }
+        }
+        let editedResult = result.slice(0, -1);
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        returnString = `<h1>I'm currently calibrating the ${editedResult}.</h1>`;
+    } else if (splitUrl) {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        returnString = newTemplate;
+    }
+    res.end(returnString + `Counter currently at ${counter}.`);
+});
+
+server.listen(3000, () => {
+    console.log("Server listening at port 3000");
+});
